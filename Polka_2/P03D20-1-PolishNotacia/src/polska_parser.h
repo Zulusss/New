eda27468c7f14b96bf551e7c4812a160808957ff
Zulusss <grivazol@gmail.com>
2024-02-24 19:06:08 +0700

    Third

---
+++ Polka_2/P03D20-1-PolishNotacia/src/polska_parser.h
@@ -0,0 +1,8 @@
+//#ifndef LOCAL_REPOS_P03D20_0_SRC_POLSKA_PARCER_H_
+//#define LOCAL_REPOS_P03D20_0_SRC_POLSKA_PARCER_H_
+#ifndef SRC_PARCER_H_
+#define SRC_PARCER_H_
+
+double polska_parcer(char formula[100], double x);
+
+#endif  // LOCAL_REPOS_P03D20_0_SRC_POLSKA_PARCER_H_
